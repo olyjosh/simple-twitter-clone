64f411b02ca72952af1b6f653809653f80f2712a
360campstore <suleimanolamilekan03@gmail.com>
2018-11-02 07:46:54 +0100

    Login & Reset Password Pages Added

---
+++ src/pages/login/login.js
@@ -0,0 +1,148 @@
+import React, { PureComponent } from "react";
+class loginComponent extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <link
+          rel="stylesheet"
+          href="https://abs.twimg.com/a/1540966379/css/t1/twitter_core.bundle.css"
+          class="coreCSSBundles"
+        />
+        <div className="three-col logged-out ms-windows">
+          <div className="route-login login-responsive">
+            <div className="topbar js-topbar">
+              <div
+                className="global-nav global-nav--newLoggedOut"
+                data-section-term="top_nav"
+              >
+                <div className="global-nav-inner">
+                  <div className="container">
+                    <ul
+                      className="nav js-global-actions"
+                      role="navigation"
+                      id="global-actions"
+                    >
+                      <li
+                        id="global-nav-home"
+                        className="home"
+                        data-global-action="home"
+                      >
+                        <a
+                          className="js-nav js-tooltip js-dynamic-tooltip"
+                          data-placement="bottom"
+                          onClick=""
+                          data-component-context="home_nav"
+                          data-nav="home"
+                        >
+                          <span className="Icon Icon--bird Icon--large" />
+                          <span className="text" aria-hidden="true">
+                            Home
+                          </span>
+                          <span className="u-hiddenVisually a11y-inactive-page-text">
+                            Home
+                          </span>
+                          <span className="u-hiddenVisually a11y-active-page-text">
+                            Home, current page.
+                          </span>
+                        </a>
+                      </li>
+                      <li id="global-nav-about" className="about">
+                        <a
+                          className="js-tooltip js-dynamic-tooltip"
+                          data-placement="bottom"
+                          onClick=""
+                          rel="noopener"
+                        >
+                          <span className="text">About</span>
+                        </a>
+                      </li>
+                    </ul>
+                    <div className="pull-right nav-extras" />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div id="page-outer">
+              <div
+                id="page-container"
+                className="AppContent wrapper wrapper-login"
+              >
+                <div className="page-canvas">
+                  <div className="signin-wrapper" data-login-message="">
+                    <h1>Log in to Simple Twitter Clone</h1>
+                    <div className="t1-form clearfix signin js-signin">
+                      <fieldset>
+                        <legend className="visuallyhidden">Log in</legend>
+
+                        <div className="clearfix field">
+                          <input
+                            className="js-username-field email-input js-initial-focus"
+                            type="text"
+                            name="username"
+                            placeholder="Phone, email or username"
+                          />
+                        </div>
+                        <div className="clearfix field">
+                          <input
+                            className="js-password-field"
+                            type="password"
+                            name="password"
+                            placeholder="Password"
+                          />
+                        </div>
+                      </fieldset>
+
+                      <div className="clearfix">
+                        <button
+                          onClick=""
+                          className="submit EdgeButton EdgeButton--primary EdgeButtom--medium"
+                        >
+                          Log in
+                        </button>
+
+                        <div className="subchck">
+                          <label className="t1-label remember">
+                            <input
+                              type="checkbox"
+                              value="1"
+                              name="remember_me"
+                              checked="false"
+                            />
+                            Remember me
+                            <span className="separator"> · </span>
+                            <a className="forgot" onClick="">
+                              Forgot password?
+                            </a>
+                          </label>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="clearfix mobile has-sms">
+                    <p className="signup-helper">
+                      New to Twitter?
+                      <a id="login-signup-link" onClick="">
+                        Sign up now &#32; &raquo;
+                      </a>
+                    </p>
+                    <p className="sms-helper">
+                      Already using Twitter via text message?
+                      <a onClick="">Activate your account &#32; &raquo;</a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default loginComponent;
---
+++ src/pages/reset-password/reset-password.js
@@ -0,0 +1,61 @@
+import React, { PureComponent } from "react";
+class resetPasswordComponent extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <link
+          href="https://abs-0.twimg.com/login/password-reset.ef5620c0a0af23edaa5965591adf636e2e1669e4.css"
+          rel="stylesheet"
+        />
+        <div className="ResponsiveLayout">
+          <div className="TopNav">
+            <div className="TopNav--container u-cf">
+              <div class="Icon--logo u-pullLeft">
+                <img src={require("../../images/twitter.png")} alt="twitter" />
+              </div>
+              <div className="TopNav-title u-pullLeft">
+                <strong>Password Reset</strong>
+              </div>
+            </div>
+          </div>
+          <div className="PageContainer">
+            <div className="Section">
+              <div className="PageHeader Edge">
+                Find your Simple Twitter Clone account
+              </div>
+              <p>Enter your email, phone number, or username.</p>
+
+              <div className="Form">
+                <input
+                  type="text"
+                  name="account_identifier"
+                  className="Form-textbox Edge-textbox is-required is-validatedRemotely"
+                  value=""
+                  autofocus
+                  autocorrect="off"
+                  autocapitalize="off"
+                />
+                <span className="Form-message">
+                  <span data-key="required">
+                    We need this information to find your account.
+                  </span>
+                </span>
+                <input
+                  type="submit"
+                  className="Button EdgeButton--primary EdgeButton"
+                  onClick=""
+                  value="Search"
+                />
+              </div>
+              <div className="Section-footer" />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default resetPasswordComponent;
