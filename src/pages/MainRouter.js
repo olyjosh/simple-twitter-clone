dadc4f864c9f64de4d916f0c0b1e2a7bcd868151
Thommzy <tomiwaobeisun@yahoo.com>
2018-11-05 17:36:29 +0100

    Register Page Done

--- src/pages/MainRouter.js
+++ src/pages/MainRouter.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Login from "./login/login";
 import App from "../App";
+import Register from "./register/register";
 import ResetPassword from "./reset-password/reset-password";
 import ConfirmPassword from "./confirm-password/confirm-password";
 const MainRouter = () => (
@@ -11,6 +12,7 @@ const MainRouter = () => (
       <Route path="/login" component={Login} />
       <Route path="/reset-password" component={ResetPassword} />
       <Route path="/confirm-password" component={ConfirmPassword} />
+      <Route path="/register" component={Register} />
     </Switch>
   </main>
 );
